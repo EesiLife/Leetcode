1c3d2623598d36f7fdf87d38e4bc6adc9deb8ef5
siyu <siyu@zhizhangyi.com>
2018-06-09 18:23:18 +0800

    modify 824

--- lib/src/main/java/com/eesilife/lib/L303RangeSumQueryImmutable.java
+++ lib/src/main/java/com/eesilife/lib/L303RangeSumQueryImmutable.java
@@ -1,7 +1,37 @@
 package com.eesilife.lib;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * Created by siy on 18-6-5.
  */
 public class L303RangeSumQueryImmutable {
+
+    public class NumArray {
+        private HashMap<String, Integer> map = new HashMap<>();
+        private int[] arr;
+        private int sum;
+        public NumArray(int[] nums) {
+            arr  = nums;
+        }
+
+        public int sumRange(int i, int j) {
+            sum = 0;
+            if (i > j)return 0;
+            for (int k = i; k < j+1 ; k++){
+                sum+= arr[k];
+            }
+            return sum;
+        }
+    }
+
+    public static void main(String[] args){
+//        int[] a = new int[]{0,1,2,3,4,5,6};
+        int[] a = new int[]{-2, 0, 3, -5, 2, -1};
+        L303RangeSumQueryImmutable c = new L303RangeSumQueryImmutable();
+        L303RangeSumQueryImmutable.NumArray n = c.new NumArray(a);
+        System.out.println(n.sumRange(2, 5));
+    }
 }

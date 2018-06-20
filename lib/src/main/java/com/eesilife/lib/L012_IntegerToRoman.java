4dd3c3917478f28283b9178d20ab50e138e38af0
EesiLife <siyu3223@qq.com>
2018-06-20 23:12:27 +0800

    add 012.

---
+++ lib/src/main/java/com/eesilife/lib/L012_IntegerToRoman.java
@@ -0,0 +1,21 @@
+package com.eesilife.lib;
+
+public class L012_IntegerToRoman {
+
+    public String intToRoman(int num) {
+        int[] base = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] str = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+                "V", "IV", "I" };
+        String roman = "";
+        int i = 0;
+        while (num != 0) {
+            if (num >= base[i]) {
+                num -= base[i];
+                roman += str[i];
+            } else{
+                i++;
+            }
+        }
+        return roman;
+    }
+}

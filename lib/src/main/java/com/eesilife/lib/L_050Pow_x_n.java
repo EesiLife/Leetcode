b2b5242e674bcb19934e1a46e0b8657fcb96d0b6
siyu <siyu@zhizhangyi.com>
2018-06-27 10:21:18 +0800

    add 050

--- lib/src/main/java/com/eesilife/lib/L_050Pow_x_n.java
+++ lib/src/main/java/com/eesilife/lib/L_050Pow_x_n.java
@@ -1,32 +1,21 @@
-package com;
+package com.eesilife.lib;
 
 import java.util.Stack;
 
-public class T2 {
+/**
+ * Created by siy on 18-6-27.
+ */
+public class L_050Pow_x_n {
     public double myPow(double x, int n) {
-        if (n < 0)
-            return 1 / x * myPow(1 / x, -(n + 1));
-        else if (n == 0)
-            return 1;
-        else if (n == 1)
-            return x;
-
-        double half = myPow(x, n >> 1);
-        half *= half;
-
-        if ((n & 1) == 1)
-            half *= x;
-        return half;
-    }
-
-    double myPow3(double x, int n) {
         if (n == 0)
             return 1;
         if (n < 0)
-            return myPow3(1 / x, -(n + 1)) / x;
+            return myPow(1 / x, -(n + 1)) / x;
         Stack<Integer> s = new Stack<Integer>();
-        for (; n != 1; n /= 2)
+        for (; n != 1; n /= 2){
             s.push(n);
+            System.out.println(n);
+        }
         double t = x;
         while (!s.isEmpty()) {
             t *= t;
@@ -35,13 +24,12 @@ public class T2 {
         }
         return t;
     }
-
-    public static void main(String[] args) {
-        T2 c = new T2();
+    public static void main(String[] args){
+        L_050Pow_x_n c = new L_050Pow_x_n();
         long time = System.currentTimeMillis();
 //        System.out.println(c.myPow(0.00001, 2147483647));
 //        System.out.println(c.myPow(-2.00000, 2));
-        System.out.println(c.myPow3(2.00000, -2147483648));
+        System.out.println(c.myPow(2.00000, 777));
         System.out.println((System.currentTimeMillis() - time) / 1000);
     }
 }

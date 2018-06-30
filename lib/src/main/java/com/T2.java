14e461f7d94bddcb7d002a46a5504ab6647fff49
EesiLife <siyu3223@qq.com>
2018-07-01 00:17:56 +0800

    add test.

--- lib/src/main/java/com/T2.java
+++ lib/src/main/java/com/T2.java
@@ -41,7 +41,30 @@ public class T2 {
         long time = System.currentTimeMillis();
 //        System.out.println(c.myPow(0.00001, 2147483647));
 //        System.out.println(c.myPow(-2.00000, 2));
-        System.out.println(c.myPow3(2.00000, -2147483648));
-        System.out.println((System.currentTimeMillis() - time) / 1000);
+//        System.out.println(c.myPow3(2.00000, -2147483648));
+//        System.out.println((System.currentTimeMillis() - time) / 1000);
+
+        System.out.println(c.isAnagram("", ""));
+//        System.out.println(c.isAnagram("aab", "aac"));
+    }
+
+    public int findContentChildren(int[] g, int[] s) {
+
+        for (int i = 0; i < s.length; i++) {
+//            for (int j = 0; j < g.length; j++){
+//                if (s[i] > = )
+//            }
+        }
+        return 0;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s == t)return true;
+        for (int i = 0; i < s.length() - 1; i++) {
+            String a = String.valueOf(s.charAt(i+1)) + String.valueOf(s.charAt(i));
+            if (t.contains(a))
+                return true;
+        }
+        return false;
     }
 }

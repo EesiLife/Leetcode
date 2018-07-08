37393b1b708984062a9cbe163ed2b5c4e2c4beb8
EesiLife <siyu3223@qq.com>
2018-07-08 15:06:19 +0800

    add 374.

--- lib/src/main/java/com/eesilife/lib/L374_GuessNumberHigherOrLower.java
+++ lib/src/main/java/com/eesilife/lib/L374_GuessNumberHigherOrLower.java
@@ -2,16 +2,15 @@ package com.eesilife.lib;
 
 import com.eesilif.lib.entity.GuessGame;
 
-public class L374_GuessNumberHigherOrLower extends GuessGame{
-    int sum = 0;
+public class L374_GuessNumberHigherOrLower extends GuessGame {
     public int guessNumber(int n) {
-        int low=1;
-        while(low<=n){
-            int mid=low+(n-low)/2;
-            int res=guess(mid);
-            if(res==0)  return mid;
-            else if(res==-1)    n=mid-1;
-            else    low=mid+1;
+        int low = 1;
+        while (low <= n) {
+            int mid = low + (n - low) / 2;
+            int temp = guess(mid);
+            if (temp == 0) return mid;
+            else if (temp == -1) n = mid - 1;
+            else low = mid + 1;
         }
         return -1;
     }

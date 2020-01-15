5cad1af62227095b2b23995c6724c0b67dba81cc
siyu <siyu@zhizhangyi.com>
2020-01-15 17:54:40 +0800

    update L035

--- lib/src/main/java/com/eesilife/lib/L035_SearchInsertPosition.java
+++ lib/src/main/java/com/eesilife/lib/L035_SearchInsertPosition.java
@@ -13,4 +13,19 @@ public class L035_SearchInsertPosition {
         if(index == -1)return nums.length;
         return -1;
     }
+
+    public int searchInsert1(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target)
+                high = mid - 1;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                return mid;
+        }
+        return low;
+    }
 }

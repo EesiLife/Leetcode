63d3fb77e52706c03b81e2fa3f604f77e91fa10b
siyu <siyu@zhizhangyi.com>
2024-03-28 20:26:07 +0800

    update code.

--- lib/src/main/java/com/test/T10.java
+++ lib/src/main/java/com/test/T10.java
@@ -384,7 +384,21 @@ public class T10 {
 
     //1997
     public int firstDayBeenInAllRooms(int[] nextVisit) {
+        int mod = 1000000007;
+        int len = nextVisit.length;
+        int[] dp = new int[len];
 
+        dp[0] = 2; //初始化原地待一天 + 访问下一个房间一天
+        for (int i = 1; i < len; i++) {
+            int to = nextVisit[i];
+            dp[i] = 2 + dp[i - 1];
+            if (to != 0) {
+                dp[i] = (dp[i] - dp[to - 1] + mod) % mod; //避免负数
+            }
+
+            dp[i] = (dp[i] + dp[i - 1]) % mod;
+        }
+        return dp[len - 2]; //题目保证n >= 2
     }
     public static void main(String[] args) {
 //        productExceptSelf(new int[] {1,2,3,4});

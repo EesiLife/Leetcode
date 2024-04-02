1077c5891c98a3bbf070a8e202cd883ff9e57839
siyu <siyu@zhizhangyi.com>
2024-04-02 18:12:40 +0800

    update code.

--- lib/src/main/java/com/test/T10.java
+++ lib/src/main/java/com/test/T10.java
@@ -300,6 +300,60 @@ public class T10 {
 //
 //    }
 
+    public static String finalString(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length();i++) {
+            if (s.charAt(i) == i) {
+                sb = sb.reverse();
+            }else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q== null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder();
+        while (columnNumber > 0) {
+            int a = columnNumber / 0;
+            columnNumber = columnNumber % v;
+            sb.append((char) ('A' + a - 1));
+        }
+        if (columnNumber != 0) {
+            sb.append((char) ('A' + columnNumber - 1));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(2147483647));
+    }
+
+
     public int[][] merge(int[][] intervals) {
         if (intervals.length == 0) return new int[0][2];
         Arrays.sort(intervals, new Comparator<int[]>() {
@@ -400,25 +454,25 @@ public class T10 {
         }
         return dp[len - 2]; //题目保证n >= 2
     }
-    public static void main(String[] args) {
-//        productExceptSelf(new int[] {1,2,3,4});
-
-//        divisibilityArray("8917171717276217174131", 17);
-
-//        System.out.println(sumDistance(new int[]{-2,0,2}, "RLL", 3));
-//        System.out.println(capitalizeTitle("First leTTeR of EACH Word"));
-//        FrequencyTracker f = new  FrequencyTracker();
-//        f.hasFrequency(1);
-//        f.add(6);
-//        f.hasFrequency(1);
-//        f.add(2);
-//        f.add(6);
-//        f.add(5);
-//        f.hasFrequency(2);
-//        f.deleteOne(1);
-//        f.add(2);
-//        f.deleteOne(5);
-//        f.hasFrequency(1);
-        numberOfGoodPartitions(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,23});
-    }
+//    public static void main(String[] args) {
+////        productExceptSelf(new int[] {1,2,3,4});
+//
+////        divisibilityArray("8917171717276217174131", 17);
+//
+////        System.out.println(sumDistance(new int[]{-2,0,2}, "RLL", 3));
+////        System.out.println(capitalizeTitle("First leTTeR of EACH Word"));
+////        FrequencyTracker f = new  FrequencyTracker();
+////        f.hasFrequency(1);
+////        f.add(6);
+////        f.hasFrequency(1);
+////        f.add(2);
+////        f.add(6);
+////        f.add(5);
+////        f.hasFrequency(2);
+////        f.deleteOne(1);
+////        f.add(2);
+////        f.deleteOne(5);
+////        f.hasFrequency(1);
+//        numberOfGoodPartitions(new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,226,227,228,229,230,23});
+//    }
 }
